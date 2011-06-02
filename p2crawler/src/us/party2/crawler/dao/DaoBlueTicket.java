880454c1cd8a0e87efeefda4b79c5a55b284a016
webverse.systems <webverse.systems@gmail.com>
2011-06-02 13:36:31 +0000

    dirty structure ajusted

---
+++ p2crawler/src/us/party2/crawler/dao/DaoBlueTicket.java
@@ -0,0 +1,25 @@
+package us.party2.crawler.dao;
+
+import us.party2.crawler.adapter.IDaoAdapter;
+
+public class DaoBlueTicket implements IDaoAdapter {
+
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void insert() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ p2crawler/src/us/party2/crawler/parser/ParserBlueTicket.java
@@ -0,0 +1,13 @@
+package us.party2.crawler.parser;
+
+import us.party2.crawler.adapter.IParserAdapter;
+
+public class ParserBlueTicket implements IParserAdapter {
+
+	@Override
+	public void parse() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
