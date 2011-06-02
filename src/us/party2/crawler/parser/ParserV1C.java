292954d10ea3b461eb75549713fcdee9f28cf29d
webverse.systems <webverse.systems@gmail.com>
2011-06-02 12:47:55 +0000

    p2us crawler src

---
+++ src/us/party2/crawler/parser/ParserV1C.java
@@ -0,0 +1,12 @@
+package us.party2.crawler.parser;
+
+import us.party2.crawler.adapter.IParserAdapter;
+
+public class ParserV1C implements IParserAdapter {
+
+	@Override
+	public void parse() {
+
+	}
+
+}
