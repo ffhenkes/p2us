a85fb0cceda35c0b91b57a0d692ccd7f8f3626a5
webverse.systems@gmail.com <webverse.systems@gmail.com@fb8d214c-6b8f-bd14-4115-ac9b519496c3>
2011-06-02 18:40:43 +0000

    parser test

--- trunk/p2crawler/src/us/party2/crawler/Main.java
+++ trunk/p2crawler/src/us/party2/crawler/Main.java
@@ -1,6 +1,7 @@
 package us.party2.crawler;
 
 import us.party2.crawler.factory.SocketFactory;
+import us.party2.crawler.parser.ParserV1C;
 
 public class Main {
 
@@ -10,11 +11,14 @@ public class Main {
 	public static void main(String[] args) {
 		
 		//desenhar workflow 
-		
-		
 		SocketFactory sock = new SocketFactory();
-		sock.getData();
+		String sb = sock.getData();
 		
+		if (sb.length() > 0) {
+			ParserV1C parserVC1 = new ParserV1C();
+			parserVC1.setResponse(sb);
+			parserVC1.parse();
+		}
 		
 		
 	}
