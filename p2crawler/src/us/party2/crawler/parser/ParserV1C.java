2af7efbbc45b475cab37a6c8b540593adfb214d4
juliano.gaio <juliano.gaio@gmail.com>
2011-06-06 04:37:11 +0000

    Added V1C model bean class. Updated V1C Parser, parsing general information blocks.

--- p2crawler/src/us/party2/crawler/parser/ParserV1C.java
+++ p2crawler/src/us/party2/crawler/parser/ParserV1C.java
@@ -1,6 +1,8 @@
 package us.party2.crawler.parser;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
 
 import org.apache.commons.lang.StringUtils;
 import org.htmlcleaner.CleanerProperties;
@@ -10,6 +12,7 @@ import org.htmlcleaner.PrettyXmlSerializer;
 import org.htmlcleaner.TagNode;
 
 import us.party2.crawler.adapter.IParserAdapter;
+import us.party2.crawler.bean.BeanV1C;
 
 public class ParserV1C implements IParserAdapter {
 
@@ -17,30 +20,37 @@ public class ParserV1C implements IParserAdapter {
 	
 	@Override
 	public void parse() {
-		//.replaceAll("\\<.*?>", "")
-		StringUtils.deleteWhitespace(response);
+	
+		ArrayList<String> blocks = new ArrayList();
+		ArrayList<TagNode> nodes = new ArrayList();
 		
-		//System.out.println("parse1>> "+response.indexOf("class=\"mlink\">"));
-		//System.out.println("parse2>> "+response.indexOf("OFERTA DO DIA:"));
+		//extrai centro da pagina
+		String kernel = response.substring(response.indexOf("<TD bgcolor=\"#F7F1DE\" align=\"left\" colspan=\"2\">", 0), response.lastIndexOf("<HR size=\"1\" color=\"#EDD28F\" width=\"95%\">"));
 		
-		response = response.substring(response.indexOf("class=\"mlink\">"), response.indexOf("OFERTA DO DIA:"));
-		
-		System.out.println(response.substring(response.indexOf("B>") + 2 //length do token
-				, response.indexOf("</B>")));
-		
-		
-		//System.out.println("parser test>>"+response);
-		
+		//System.out.println(kernel);
+		//enqto tiver bloco de interesse, extrai bloco e insere no array de blocos
+		do{						
+			String block = kernel.substring(kernel.indexOf("<TD bgcolor=\"#F7F1DE\" align=\"left\" colspan=\"2\">"),
+						kernel.indexOf("<HR size=\"1\" color=\"#EDD28F\" width=\"95%\">"));	
+			blocks.add(block);
+			//System.out.println(block);
+			
+			HtmlCleaner hc = new HtmlCleaner();
+			CleanerProperties props = hc.getProperties();
+			props.setTranslateSpecialEntities(true);
+			props.setTransResCharsToNCR(true);
+			props.setOmitComments(true);
+			
+			TagNode node = hc.clean(block);
+			nodes.add(node);
+						
+			kernel = kernel.substring(kernel.indexOf("<TD bgcolor=\"#F7F1DE\" align=\"left\" colspan=\"2\">",200));
+			//System.out.println(kernel);
+			
+		}while(kernel.indexOf("<TD bgcolor=\"#F7F1DE\" align=\"left\" colspan=\"2\">",200)>0);
+			
+		parseBlocks(blocks);
 		/*
-		HtmlCleaner hc = new HtmlCleaner();
-		CleanerProperties props = hc.getProperties();
-		props.setTranslateSpecialEntities(true);
-		props.setTransResCharsToNCR(true);
-		props.setOmitComments(true);
-		TagNode node = hc.clean(response);
-		
-		
-		
 		//TagNode[] nodes = node.getElementsByName("", true);
 		try {
 			new PrettyXmlSerializer(props).writeToFile(node, "/home/fabio/Develop/parseado.xml", "utf-8");
@@ -50,15 +60,9 @@ public class ParserV1C implements IParserAdapter {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} 
-		*/
-		
-		//System.out.println("parser>> "+response);
-		
-		//String titulo = response.substring(response.indexOf(" class=\"mlink\"><B>"), response.indexOf("</B></A></TD>"));
-		//System.out.println(titulo);
-		
+		*/			
 	}
-
+		
 	public String getResponse() {
 		return response;
 	}
@@ -67,4 +71,19 @@ public class ParserV1C implements IParserAdapter {
 		this.response = response;
 	}
 
+	private void parseBlocks(ArrayList<String> blocks){
+		ArrayList<BeanV1C> beans = new ArrayList<BeanV1C>(); 
+		
+		for (int i = 0; i < blocks.size(); i++) {  
+			BeanV1C bean = new BeanV1C();
+            String block = blocks.get(i);  
+            
+            String event = block.substring(block.indexOf("class=\"mlink\"><B>"), block.indexOf("</B></A></TD>",block.indexOf("class=\"mlink\"><B>")));
+            bean.setPartyName(event);
+            String atracao = block.substring(block.indexOf("<FONT class=\"topicoAgenda\">Atrações:"), block.indexOf("</B></A></TD>",block.indexOf("<FONT class=\"topicoAgenda\">Atrações:")));
+            bean.setPartyDetail(atracao);
+            String date = block.substring(block.indexOf("<TD><FONT class=\"topicoAgenda\"></FONT><FONT class=\"tpmedio\">"), block.indexOf("</FONT></TD>",block.indexOf("<TD><FONT class=\"topicoAgenda\"></FONT><FONT class=\"tpmedio\">")));
+            bean.setPartyDate(date);
+		} 
+	}
 }
