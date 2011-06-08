aac5ac0d14439b4a67763160d17467a8db0fefe3
juliano.gaio@gmail.com <juliano.gaio@gmail.com@fb8d214c-6b8f-bd14-4115-ac9b519496c3>
2011-06-08 03:44:00 +0000

    DAO implementation Start-up (factory, crud mockups)

--- trunk/p2crawler/src/us/party2/crawler/factory/ConnectionFactory.java
+++ trunk/p2crawler/src/us/party2/crawler/factory/ConnectionFactory.java
@@ -1,5 +1,22 @@
 package us.party2.crawler.factory;
 
-public class ConnectionFactory {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class ConnectionFactory {
+	
+	public static Connection getConnection() throws SQLException{
+		try {
+			Class.forName("org.postgresql.Driver");
+			//usa
+			//return (Connection) DriverManager.getConnection("jdbc:postgresql://209.62.12.130:5432/usa?user=elenice&password=elenice_2011");
+
+			//postos
+			return (Connection) DriverManager.getConnection("jdbc:postgresql://localhost:5432/P2Us?user=juliano&password=regor289c");
+			
+		} catch (ClassNotFoundException e) {
+			throw new SQLException(e.getMessage());
+		}
+	}
 }
