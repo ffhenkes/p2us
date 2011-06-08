a4917f2816147c7a62234e8c746896d5682da17e
juliano.gaio <juliano.gaio@gmail.com>
2011-06-08 03:44:00 +0000

    DAO implementation Start-up (factory, crud mockups)

--- p2crawler/src/us/party2/crawler/adapter/IDaoAdapter.java
+++ p2crawler/src/us/party2/crawler/adapter/IDaoAdapter.java
@@ -1,9 +1,11 @@
 package us.party2.crawler.adapter;
 
+import java.sql.SQLException;
+
 public interface IDaoAdapter {
 
-	public void insert();
-	public void update();
-	public void delete();
+	public void insert() throws SQLException;
+	public void update() throws SQLException;
+	public void delete() throws SQLException;
 	
 }
--- p2crawler/src/us/party2/crawler/dao/DaoV1C.java
+++ p2crawler/src/us/party2/crawler/dao/DaoV1C.java
@@ -1,25 +1,51 @@
 package us.party2.crawler.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import us.party2.crawler.adapter.IDaoAdapter;
 
 public class DaoV1C implements IDaoAdapter{
 
+	Connection conn = null;
+	
 	@Override
-	public void delete() {
-		// TODO Auto-generated method stub
+	public void delete() throws SQLException {
+		conn = us.party2.crawler.factory.ConnectionFactory.getConnection();
 		
+		//MOCKUP
+		String sql = "delete from v1c where id='1'";
+		PreparedStatement ps = conn.prepareStatement(sql);
+		if(ps.executeUpdate()>0){
+			System.out.println("deleted >> count: " + ps.getUpdateCount());
+		}
 	}
 
 	@Override
-	public void insert() {
-		// TODO Auto-generated method stub
+	public void insert() throws SQLException {
+		conn = us.party2.crawler.factory.ConnectionFactory.getConnection();
 		
+		//MOCKUP
+		String sql = "insert into v1c(party,date) values(?, ?)";
+		
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+	    pstmt.setString(1, "party"); 
+	    pstmt.setString(2, "09/06/2011"); 
+	      
+	    pstmt.execute();
 	}
 
 	@Override
-	public void update() {
-		// TODO Auto-generated method stub
+	public void update() throws SQLException {
+		conn = us.party2.crawler.factory.ConnectionFactory.getConnection();
 		
+		//MOCKUP
+		String sql = "update v1c set party='wow' where id='1'";
+		PreparedStatement ps = conn.prepareStatement(sql);
+		if(ps.executeUpdate()>0){
+			System.out.println("updated >> count: " + ps.getUpdateCount());
+		}
 	}
 
 }
